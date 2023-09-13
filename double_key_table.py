0d7419791f3b7f1047ccae69e264989705979348
Giganotosaurus <75021575+joshrkhoo@users.noreply.github.com>
2023-09-14 00:57:41 +1000

    Add documentation, TODO: Add complexity

--- double_key_table.py
+++ double_key_table.py
@@ -39,8 +39,7 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         self.sizes = sizes or self.TABLE_SIZES
         self.internal_sizes = internal_sizes or self.TABLE_SIZES
-        # self.size_index = 0
-        # self.array:ArrayR[K1, LinearProbeTable[K2, V]] = ArrayR(self.sizes[self.size_index])
+
         self.count = 0
 
         self.outer_table: LinearProbeTable[K1, LinearProbeTable[K2, V]] = LinearProbeTable(self.sizes)
@@ -83,86 +82,43 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         :raises KeyError: When the key pair is not in the table, but is_insert is False.
         :raises FullError: When a table is full and cannot be inserted.
         """
+
+        # If statement checks if outer table has key1 and if it doesnt then we either add it or raise a key error
         if key1 not in self.outer_table:
             if is_insert:
+                # if is_insert is True and there is no inner table for key1, create a new inner table
                 new_inner_table = LinearProbeTable(self.internal_sizes)
+                # set the hash function of the new inner table to be the hash2 function
                 new_inner_table.hash = lambda k: self.hash2(k, new_inner_table)
+                # insert the new inner table into the outer table at key1
+                    # essentially, key 1 and the new inner table get inserted into the outer table
+                    # key1 just points to the new inner table
                 self.outer_table[key1] = new_inner_table
             else:
                 raise KeyError(key1)
+        
+        # get the inner table for key1
         sub_table = self.outer_table[key1]
+
+        # return the position of key1 and key2
+            # Uses the linear probe function implemented in the LinearProbeTable class
         return (
             self.outer_table._linear_probe(key1, False),
             sub_table._linear_probe(key2, is_insert)
         )
-
-
-
-
-    #     # Position1 is the position of the top-level key
-    #     position1 = self.hash1(key1)
-    #     # print(position1)
-
-    #     # sub_table is the bottom-level hash table for the top-level key   
-    #     sub_table = self.array[position1]
-
-    #     for _ in range(self.table_size):
-    #         # No sub-table exists for the top-level key
-    #         if sub_table is None:
-    #             if is_insert:
-    #                 # if is_insert is True and there is no, create a new sub-table
-    #                 sub_table = LinearProbeTable(self.internal_sizes)
-    #                 sub_table.hash = lambda k: self.hash2(k, sub_table)
-    #             else:
-    #                 raise KeyError
             
-
-    
-
-    #     # Position2 is the position of the bottom-level key
-    #     position2 = self.hash2(key2, sub_table)
-    #     # print(position2)
-    #     # Linear probe through the bottom-level hash table
-    #     for _ in range(sub_table.table_size):
-    #         if sub_table.array[position2] is None:
-    #             # Empty spot. Am I upserting or retrieving?
-    #             if is_insert:
-    #                 return (position1, position2)
-    #             else:
-    #                 raise KeyError(key2)
-    #         elif sub_table.array[position2][0] == key2:
-    #             return (position1, position2)
-    #         else:
-    #             # Taken by something else. Time to linear probe.
-    #             position2 = (position2 + 1) % sub_table.table_size
-
-        
-        
-
     def keys(self, key:K1|None=None) -> list[K1|K2]:
         """
         key = None: returns all top-level keys in the table.
         key = x: returns all bottom-level keys for top-level key x.
         """
+
         if key:
+            # keys() gets all the keys in the hash table
+                # here we get all the keys at the inner hash table which is pointed to by key
             return self.outer_table[key].keys()
+        # if key is None, return all the keys in the outer hash table
         return self.outer_table.keys()
-
-
-        # if key is None:
-        #     return [k1 for k1 in self.iter_keys()]
-        # else:
-        #     # Position1 is the position of the top-level key
-        #     position1 = self.hash1(key)
-        #     # sub_table is the bottom-level hash table for the top-level key
-        #     sub_table = self.array[position1]
-        #     # Iterate through the bottom-level hash table and return all keys
-        #     if sub_table is not None:
-        #         return [k2 for k2 in sub_table.iter_keys()]
-        #     else:
-        #         return [] 
-
-
 
 
     def iter_keys(self, key:K1|None=None) -> Iterator[K1|K2]:
@@ -176,20 +132,28 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         """
         # If key is None, iterate through all top-level keys
         if key is None:
+            # Iterate through all top-level keys
+            # use array attribute of outer table to iterate through all the keys
             for tup in self.outer_table.array:
+                # if the key is not None, yield the key
+                # yield is like return but it returns a generator
                 if tup is not None:
                     yield tup[0]
 
 
         # Else, iterate through all bottom-level keys for top-level key k
         else:
+            # if key is not in the outer table, raise a key error
             if key not in self.outer_table:
                 raise KeyError(key)
 
+            # get the inner table for key
             sub_table = self.outer_table[key]
 
+            # iterate through the inner table
             for tup in sub_table.array:
                 if tup is not None:
+                    # tup[0] is the key tup[1] is the value
                     yield tup[0]
 
     def iter_values(self, key:K1|None=None) -> Iterator[V]:
@@ -199,22 +163,30 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         key = k:
             Returns an iterator of all values in the bottom-hash-table for k.
         """
+        # Get all values in all inner tables
         # If key is None, iterate through all top-level keys
         if key is None:
+            # Iterate through all top-level keys
             for outer_tup in self.outer_table.array:
                 if outer_tup is not None:
+                    # Iterate through all inner tables
+                    # outer_tup[1] is the inner table
                     for inner_tup in outer_tup[1].array:
                         if inner_tup is not None:
                             yield inner_tup[1]
 
 
-        # Else, iterate through all bottom-level keys for top-level key k
+        # Get all values in the single inner table pointed to by key
+        # Else, iterate through the bottom hash table pointed to by key
         else:
+            # if key is not in the outer table, raise a key error
             if key not in self.outer_table:
                 raise KeyError(key)
 
+            # get the inner table for key
             sub_table = self.outer_table[key]
 
+            # iterate through the inner table
             for tup in sub_table.array:
                 if tup is not None:
                     yield tup[1]
@@ -224,23 +196,19 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         key = None: returns all values in the table.
         key = x: returns all values for top-level key x.
         """
+
+        # Get all values at the inner table pointed to by key
         if key:
             return self.outer_table[key].values()
         
+        # Get all values in all inner tables
         values = []
+        # Iterate through all inner tables
         for sub_table in self.outer_table.values():
+            # add all values in the inner table to the values list
             values.extend(sub_table.values())
         return values
 
-        # if key is None:
-        #     return [v for v in self.iter_values()]
-        # else:
-        #     position1 = self.hash1(key)
-        #     sub_table = self.outer_table[position1]
-        #     if sub_table is not None:
-        #         return [v for v in sub_table.iter_values()]
-        #     else:
-        #         return []
 
 
     def __contains__(self, key: tuple[K1, K2]) -> bool:
@@ -262,6 +230,8 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         :raises KeyError: when the key doesn't exist.
         """
+
+        # K1 = key[0], K2 = key[1]
         k1, k2 = key
 
         # KeyError is raised by the LinearProbeTable class
@@ -274,35 +244,22 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         :raises FullError: when the table cannot be resized further.
         """
+
         k1, k2 = key
 
+        # If key1 is not in the outer table, create a new inner table for key1
         if k1 not in self.outer_table:
             self.outer_table[k1] = LinearProbeTable(self.internal_sizes)
             self.outer_table[k1].hash = lambda k: self.hash2(k, self.outer_table[k1])
 
+        # Get the inner table for key1
         sub_table = self.outer_table[k1]
+        # assign the value to the inner table at key2
         sub_table[k2] = data
 
+        # Increment count of the outer table
         self.count += 1
         
-
-        # self.count += 1
-
-
-        # # Get the position of the key - tuple of 2 ints
-        # position = self._linear_probe(key[0], key[1], True)
-        
-        # if self.array[position[0]] is None:
-        #     self.count += 1
-        # self.array[position[0]] = key[0]
-        # self.array[position[0]][position[1]] = (key[1], data)
-
-        # # Resize if necessary
-        # if len(self) > self.table_size / 2:
-        #     self._rehash()
-        
-    
-
 
     def __delitem__(self, key: tuple[K1, K2]) -> None:
         """
@@ -310,41 +267,23 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         :raises KeyError: when the key doesn't exist.
         """
+
         k1, k2 = key
+        # Delete the value from the inner table at key2
         del self.outer_table[k1][k2]
+        # If the inner table is empty, delete the inner table from the outer table
         if len(self.outer_table[k1]) == 0:
             del self.outer_table[k1]
+
+        # decrement count of the outer table
         self.count -= 1
-
-    # def _rehash(self) -> None:
-    #     """
-    #     Need to resize table and reinsert all values
-
-    #     :complexity best: O(N*hash(K)) No probing.
-    #     :complexity worst: O(N*hash(K) + N^2*comp(K)) Lots of probing.
-    #     Where N is len(self)
-    #     """
-    #     self.outer_table._rehash()
-    #     # Create a new array with the next size
-    #     self.size_index += 1
-    #     new_array = ArrayR(self.TABLE_SIZES[self.size_index])
-
-    #     # Iterate through the old array and rehash all values
-    #     for sub_table in self.array:
-    #         if sub_table is not None:
-    #             for k2 in sub_table:
-    #                 pos = self._linear_probe(sub_table[k2], k2, True)
-    #                 new_array[pos[0]][pos[1]] = sub_table[k2]
-
-    #     # Assign the new array to the old array
-    #     self.array = new_array
 
     @property
     def table_size(self) -> int:
         """
         Return the current size of the table (different from the length)
         """
-        # return self.sizes[self.size_index]
+
         return self.outer_table.table_size
 
     def __len__(self) -> int:
