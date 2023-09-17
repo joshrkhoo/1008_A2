e6c0802ed383a1d6ad98760ab06952d46323a83d
Daiaye <noah.dai09@gmail.com>
2023-09-17 12:16:52 +1000

    attempted linear probe method
    worked on this over a couple days. a lot of trial and error

--- double_key_table.py
+++ double_key_table.py
@@ -28,7 +28,25 @@ class DoubleKeyTable(Generic[K1, K2, V]):
     HASH_BASE = 31
 
     def __init__(self, sizes:list|None=None, internal_sizes:list|None=None) -> None:
-        raise NotImplementedError()
+        if sizes is not None:
+            self.sizes = sizes
+        else:
+            self.sizes = self.TABLE_SIZES
+
+        # create the outer table first before checking the internal_sizes
+        self.outer_table = LinearProbeTable(self.TABLE_SIZES) # LinearProbeTable has its own self.count
+        self.outer_table.hash = lambda k: self.hash1(k) # set the outer table's hash function to hash1
+        # will allow us to use the linear probing method in LinearProbeTable
+        
+
+        # table has been made so we can replace the existing TABLE_SIZES for the internal_sizes if necessary
+        if internal_sizes is not None:
+            self.internal_sizes = internal_sizes
+        else:
+            self.internal_sizes = self.TABLE_SIZES
+
+        self.count = 0 # count for the double key table (should count all the values in all tables (top and bottom))
+        
 
     def hash1(self, key: K1) -> int:
         """
@@ -65,7 +83,102 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         :raises KeyError: When the key pair is not in the table, but is_insert is False.
         :raises FullError: When a table is full and cannot be inserted.
         """
-        raise NotImplementedError()
+        # pos1 = self.hash1(key1)
+        # pos2 = self.hash2(key2, self.TABLE_SIZES)
+
+        # for _ in range(self.count):
+        #     if self.table[pos1] is None:
+        #         if is_insert:
+        #             self.table[pos1] = LinearProbeTable(self.TABLE_SIZES)
+        #             for _ in range(len(self.table[pos1])):
+        #                 if self.table[pos1][pos2] is None:
+        #                     return (pos1, pos2)
+        #                 raise KeyError
+
+        # ***NOTE***
+        # because we are using LinearProbeTable to create the outer table, we can just use _linear_probe method 
+        # for i in range(self.count): # only need to iterate over the cluster to find the next empty space
+        #     if self.table[pos1] is None:
+        #         if is_insert:
+        #             # only create the inner table when pos1 hashes to None
+        #             inner_table = LinearProbeTable(self.TABLE_SIZES) # self.TABLE_SIZES will be the internal_sizes input or sizes (if sizes is not None) or the original TABLE_SIZES (if both internal_sizes and sizes is None)
+        #         else:
+        #             raise KeyError # raise a KeyError 'when the key pair is not in the table, but is_insert is False'
+                
+        #         pos2 = self.hash2(key2, inner_table)
+                
+        #         for j in range(inner_table.count):
+        #             inner_table._linear_probe(key2, is_insert)
+
+
+        # use linear probing on the outer table to find the position of key1
+        # the tuple at this postiion - (key1, inner table) - will have another hash table as the value of key1
+        # use linear probing on the inner table to find the position of key2
+        # the tuple at that position - (key2, value) - will be the actual value we want
+
+        # if is_insert: # also want to check if the key is not in the table already
+        #     inner_table = LinearProbeTable(self.internal_sizes)
+
+        # pos1 = self.outer_table._linear_probe(key1, is_insert) # if is_insert is True, it will return the position in the outer hash table where we should create an inner table
+        # self.outer_table[key1] = inner_table
+        # pos2 = inner_table._linear_probe(key2, is_insert)
+
+
+        # assume key1 is not in the table
+        # get pos1 using linear probing for the self.outer_table
+        # therefore self.outer_table[pos1] is None
+        # we need to create a tuple (key1, inner_table) at self.outer_table[pos1]
+        # then probe inner_table for pos2 (inner_table will be empty because we just created it)
+        # return (pos1, pos2)
+
+        # pos1 = self.outer_table._linear_probe(key1, is_insert)
+        # # if key1 not in the outer table, create the inner table
+        # inner_table = LinearProbeTable(self.internal_sizes)
+        # inner_table.hash = lambda k: self.hash2(key2, inner_table)
+        # # put this table at pos1
+        # self.outer_table[key1] = inner_table
+        # pos2 = inner_table._linear_probe(key2, is_insert)
+
+        # assume key1 is in the table
+        # get pos1
+        # update the inner table at pos1 (key1, inner_table) - update inner_table
+
+
+        # check if key1 is in the table
+        # if key1 in self.outer_table:
+        #     pos1 = self.outer_table._linear_probe(key1, is_insert)
+        #     pos2 = inner_table._linear_probe(key2, is_insert)
+        #     return (pos1, pos2)
+        
+        # if is_insert:
+        #     inner_table = LinearProbeTable(self.internal_sizes)
+        #     inner_table.hash = lambda k: self.hash2(key2, inner_table)
+        #     self.outer_table[key1] = inner_table
+        #     pos1 = self.outer_table._linear_probe(key1, is_insert)
+        #     pos2 = inner_table._linear_probe(key2, is_insert)
+        #     return (pos1, pos2)
+
+        # raise KeyError
+        # if key1 is in the table, then we already have an inner table
+
+        # check if key1 is in the outer table using the __contains__ method from LPT
+        if key1 in self.outer_table:
+            pos1 = self.outer_table._linear_probe(key1, is_insert) # get its position if key1 is in the table
+            inner_table = self.outer_table[key1]
+        elif is_insert:
+            inner_table = LinearProbeTable(self.internal_sizes) # create an inner table if key1 isn't in the table
+            inner_table.hash = lambda k: self.hash2(key2, inner_table) # set the inner table's hash function
+            self.outer_table[key1] = inner_table # using __setitem__ method from LPT
+        else:
+            raise KeyError
+
+        pos2 = inner_table._linear_probe(key2, is_insert)
+        return (pos1, pos2)
+
+
+
+
+
 
     def iter_keys(self, key:K1|None=None) -> Iterator[K1|K2]:
         """
@@ -145,6 +258,7 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         """
         raise NotImplementedError()
 
+    @property
     def table_size(self) -> int:
         """
         Return the current size of the table (different from the length)
@@ -155,6 +269,9 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         """
         Returns number of elements in the hash table
         """
+
+        # should return the total number of items across all sub-tables
+        # so return the number of values in the outer table + the number of values in the inner table        
         raise NotImplementedError()
 
     def __str__(self) -> str:
