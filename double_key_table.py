5be42abcfdbdb96de7e470954ea11a88b73ed30b
Daiaye <noah.dai09@gmail.com>
2023-09-18 21:39:21 +1000

    passed some tests
    need to fix iter methods. rehash has not been implemented yet

--- double_key_table.py
+++ double_key_table.py
@@ -34,7 +34,7 @@ class DoubleKeyTable(Generic[K1, K2, V]):
             self.sizes = self.TABLE_SIZES
 
         # create the outer table first before checking the internal_sizes
-        self.outer_table = LinearProbeTable(self.TABLE_SIZES) # LinearProbeTable has its own self.count
+        self.outer_table = LinearProbeTable(self.sizes) # LinearProbeTable has its own self.count
         self.outer_table.hash = lambda k: self.hash1(k) # set the outer table's hash function to hash1
         # will allow us to use the linear probing method in LinearProbeTable
         
@@ -46,7 +46,7 @@ class DoubleKeyTable(Generic[K1, K2, V]):
             self.internal_sizes = self.TABLE_SIZES
 
         self.count = 0 # count for the double key table (should count all the values in all tables (top and bottom))
-        
+
 
     def hash1(self, key: K1) -> int:
         """
@@ -83,67 +83,6 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         :raises KeyError: When the key pair is not in the table, but is_insert is False.
         :raises FullError: When a table is full and cannot be inserted.
         """
-        # pos1 = self.hash1(key1)
-        # pos2 = self.hash2(key2, self.TABLE_SIZES)
-
-        # for _ in range(self.count):
-        #     if self.table[pos1] is None:
-        #         if is_insert:
-        #             self.table[pos1] = LinearProbeTable(self.TABLE_SIZES)
-        #             for _ in range(len(self.table[pos1])):
-        #                 if self.table[pos1][pos2] is None:
-        #                     return (pos1, pos2)
-        #                 raise KeyError
-
-        # ***NOTE***
-        # because we are using LinearProbeTable to create the outer table, we can just use _linear_probe method 
-        # for i in range(self.count): # only need to iterate over the cluster to find the next empty space
-        #     if self.table[pos1] is None:
-        #         if is_insert:
-        #             # only create the inner table when pos1 hashes to None
-        #             inner_table = LinearProbeTable(self.TABLE_SIZES) # self.TABLE_SIZES will be the internal_sizes input or sizes (if sizes is not None) or the original TABLE_SIZES (if both internal_sizes and sizes is None)
-        #         else:
-        #             raise KeyError # raise a KeyError 'when the key pair is not in the table, but is_insert is False'
-                
-        #         pos2 = self.hash2(key2, inner_table)
-                
-        #         for j in range(inner_table.count):
-        #             inner_table._linear_probe(key2, is_insert)
-
-
-        # use linear probing on the outer table to find the position of key1
-        # the tuple at this postiion - (key1, inner table) - will have another hash table as the value of key1
-        # use linear probing on the inner table to find the position of key2
-        # the tuple at that position - (key2, value) - will be the actual value we want
-
-        # if is_insert: # also want to check if the key is not in the table already
-        #     inner_table = LinearProbeTable(self.internal_sizes)
-
-        # pos1 = self.outer_table._linear_probe(key1, is_insert) # if is_insert is True, it will return the position in the outer hash table where we should create an inner table
-        # self.outer_table[key1] = inner_table
-        # pos2 = inner_table._linear_probe(key2, is_insert)
-
-
-        # assume key1 is not in the table
-        # get pos1 using linear probing for the self.outer_table
-        # therefore self.outer_table[pos1] is None
-        # we need to create a tuple (key1, inner_table) at self.outer_table[pos1]
-        # then probe inner_table for pos2 (inner_table will be empty because we just created it)
-        # return (pos1, pos2)
-
-        # pos1 = self.outer_table._linear_probe(key1, is_insert)
-        # # if key1 not in the outer table, create the inner table
-        # inner_table = LinearProbeTable(self.internal_sizes)
-        # inner_table.hash = lambda k: self.hash2(key2, inner_table)
-        # # put this table at pos1
-        # self.outer_table[key1] = inner_table
-        # pos2 = inner_table._linear_probe(key2, is_insert)
-
-        # assume key1 is in the table
-        # get pos1
-        # update the inner table at pos1 (key1, inner_table) - update inner_table
-
-
         # check if key1 is in the table
         # if key1 in self.outer_table:
         #     pos1 = self.outer_table._linear_probe(key1, is_insert)
@@ -163,7 +102,9 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         # check if key1 is in the outer table using the __contains__ method from LPT
         if key1 in self.outer_table:
-            pos1 = self.outer_table._linear_probe(key1, is_insert) # get its position if key1 is in the table
+            # pos1 = self.outer_table._linear_probe(key1, is_insert) # get its position if key1 is in the table
+            # commented out pos1 above because otherwise it is possible for pos1 to be undefined when we try to return
+            # if key1 isn't in the table, we can just look for the available position anyway - doesn't need to be in this 'if' statement
             inner_table = self.outer_table[key1]
         elif is_insert:
             inner_table = LinearProbeTable(self.internal_sizes) # create an inner table if key1 isn't in the table
@@ -172,6 +113,7 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         else:
             raise KeyError
 
+        pos1 = self.outer_table._linear_probe(key1, is_insert)
         pos2 = inner_table._linear_probe(key2, is_insert)
         return (pos1, pos2)
 
@@ -184,14 +126,41 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         key = k:
             Returns an iterator of all keys in the bottom-hash-table for k.
         """
-        raise NotImplementedError()
+        # if key is None:
+        #     for item in self.outer_table:
+        #         if item is not None:
+        #             yield item[0]
+        # else:
+        #     inner_table = self.outer_table[key]
+        #     for item in inner_table:
+        #         if item is not None:
+        #             yield item[0]
+
+        if key is None:
+            table = self.outer_table
+        else:
+            table = self.outer_table[key]
+
+        for item in table:
+            if item is not None:
+                yield item[0]
 
     def keys(self, key:K1|None=None) -> list[K1|K2]:
         """
         key = None: returns all top-level keys in the table.
         key = x: returns all bottom-level keys for top-level key x.
         """
-        raise NotImplementedError()
+        # return list(self.iter_keys(key))
+
+        key_iterator = self.iter_keys(key)
+
+        keys = []
+        for key in key_iterator:
+            keys.append(key)
+
+        return keys
+
+            
 
     def iter_values(self, key:K1|None=None) -> Iterator[V]:
         """
@@ -200,14 +169,21 @@ class DoubleKeyTable(Generic[K1, K2, V]):
         key = k:
             Returns an iterator of all values in the bottom-hash-table for k.
         """
-        raise NotImplementedError()
+        if key is None:
+            table = self.outer_table
+        else:
+            table = self.outer_table[key]
+
+        for item in table:
+            if item is not None:
+                yield item[1]
 
     def values(self, key:K1|None=None) -> list[V]:
         """
         key = None: returns all values in the table.
         key = x: returns all values for top-level key x.
         """
-        raise NotImplementedError()
+        return list(self.iter_values(key))
 
     def __contains__(self, key: tuple[K1, K2]) -> bool:
         """
@@ -349,4 +325,10 @@ class DoubleKeyTable(Generic[K1, K2, V]):
 
         Not required but may be a good testing tool.
         """
-        raise NotImplementedError()
+        result = ""
+        for i, val in enumerate(self.outer_table.array):
+            if val is None:
+                result += f"{i}, None\n"
+                continue
+            result += f"{i}, {val[0]}, {val[1]}\n"
+        return result
