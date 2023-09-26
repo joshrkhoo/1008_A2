b6d33e945f672b025957688b218c72bfd7c91f18
Daiaye <noah.dai09@gmail.com>
2023-09-26 19:59:23 +1000

    passed task 6
    still need to do complexity

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -35,3 +35,44 @@ def _binary_search_aux(l: list[T], item: T, lo: int, hi: int) -> int:
     elif l[mid] == item:
         return mid
     raise ValueError(f"Comparison operator poorly implemented {item} and {l[mid]} cannot be compared.")
+
+
+def binary_search_mountains(mountain_list: list[T], mountain: T) -> int:
+    """
+    Utilise the binary search algorithm to find the index where a particular element would be stored.
+
+    :return: The index at which either:
+        * This item is located, or
+        * Where this item would be inserted to preserve the ordering.
+
+    :complexity:
+    Best Case Complexity: O(1), when middle index contains item.
+    Worst Case Complexity: O(log(N)), where N is the length of l.
+    """
+    return _binary_search_mountains_aux(mountain_list, mountain, 0, len(mountain_list))
+
+def _binary_search_mountains_aux(mountain_list: list[T], mountain: T, lo: int, hi: int) -> int:
+    """
+    Auxilliary method used by binary search.
+    lo: smallest index where the return value could be.
+    hi: largest index where the return value could be.
+    """
+    if lo == hi:
+        return lo
+    mid = (hi + lo) // 2
+    if mountain_list[mid].difficulty_level > mountain.difficulty_level:
+        # Item would be before mid
+        return _binary_search_mountains_aux(mountain_list, mountain, lo, mid-1)
+    elif mountain_list[mid].difficulty_level < mountain.difficulty_level:
+        # Item would be after mid
+        return _binary_search_mountains_aux(mountain_list, mountain, mid+1, hi)
+    elif mountain_list[mid].difficulty_level == mountain.difficulty_level:
+        while True:
+            if mountain_list[mid].name == mountain.name:
+                return mid
+            elif mountain_list[mid].name < mountain.name:
+                mid += 1
+            elif mountain_list[mid].name > mountain.name:
+                mid -= 1
+
+    raise ValueError(f"Comparison operator poorly implemented {mountain} and {mountain_list[mid]} cannot be compared.")
